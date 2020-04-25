627f22848b72e8e7a212586463985ef77a8b69c1
gslack-app <gslack.app@gmail.com>
2020-04-25 18:39:33 +0700

    WIP: Swagger 2.0

--- www/swagger.ts
+++ www/swagger.ts
@@ -1,3 +1,9 @@
+import { Schema } from "./interfaces";
+
+declare type InType = 'query' | 'path' | 'header' | 'cookie';
+declare type DataType = 'integer' | 'number' | 'string' | 'boolean' | 'object' | 'array';
+declare type FormatType = 'int32' | 'float' | 'double' | 'byte' | 'binary' | 'date' | 'date-time' | 'password';
+
 interface Info {
     title: string;
     description?: string;
@@ -43,7 +49,7 @@ interface PathItem {
     patch?: Operation;
     trace?: Operation;
     servers?: Server[];
-    parameters: any;
+    parameters?: any;
 }
 
 interface Operation {
@@ -52,12 +58,12 @@ interface Operation {
     description?: string;
     externalDocs?: ExternalDoc;
     operationId?: string;
-    parameters?: Parameter[] | Reference[];
-    requestBody: RequestBody | Reference;
+    parameters?: (Parameter | Reference)[];
+    requestBody?: RequestBody | Reference;
     responses: Record<'default' | string, Response>;
     // Skip `callbacks`
     deprecated?: boolean;
-    security: Record<string, string[]>;
+    security?: Record<string, string[]>;
     servers?: Server[];
 }
 
@@ -69,7 +75,7 @@ interface ExternalDoc {
 interface Parameter {
     // Fixed Fields
     name: string;
-    in: 'query' | 'path' | 'header' | 'cookie';
+    in: InType;
     description?: string;
     required?: boolean;
     deprecated?: boolean;
@@ -78,11 +84,11 @@ interface Parameter {
     style?: string;
     explode?: boolean;
     allowReserved?: boolean;
-    schema: Schema | Reference;
+    schema: SchemaObject | Reference;
     // Skip `example` & `examples` and `content` fields
 }
 
-interface Schema {
+interface SchemaObject {
     title?: string;
     multipleOf?: number;
     maximum?: number;
@@ -99,9 +105,12 @@ interface Schema {
     minProperties?: number;
     required?: string[];
     enum?: string[];
-    type?: 'integer' | 'number' | 'string' | 'boolean';
-    format?: 'int32' | 'float' | 'double' | 'byte' | 'binary' | 'date' | 'date-time' | 'password';
-    // Skip allOf, oneOf, anyOf, anyOf, not, items, properties additionalProperties, description
+    type?: DataType;
+    format?: FormatType;
+    properties?: Record<string, any>;
+    description?: string;
+    default?: any;
+    // Skip allOf, oneOf, anyOf, anyOf, not, items, additionalProperties
 }
 
 interface Reference {
@@ -122,9 +131,9 @@ interface Response {
 }
 
 interface MediaType {
-    schema: Schema | Reference;
+    schema: SchemaObject | Reference;
     // Skip `example` & `examples` fields
-    encoding: Record<string, Encoding>;
+    encoding?: Record<string, Encoding>;
 }
 
 interface Encoding {
@@ -145,7 +154,7 @@ interface Header {
     style?: string;
     explode?: boolean;
     allowReserved?: boolean;
-    schema: Schema | Reference;
+    schema: SchemaObject | Reference;
     // Skip `example` & `examples` and `content` fields
 }
 
@@ -164,6 +173,35 @@ interface Tag {
     externalDocs: ExternalDoc;
 }
 
+interface Components {
+    schemas: Record<string, SchemaObject>;
+    responses?: Record<string, Response>;
+    parameters?: Record<string, Parameter>;
+    requestBodies?: Record<string, RequestBody>;
+    headers?: Record<string, Header>;
+    securitySchemes?: Record<string, SecurityScheme>;
+    links?: Record<string, Link>;
+    // Skip `examples` & `callbacks` fields
+}
+
+interface SecurityScheme {
+    type: 'apiKey' | 'http' | 'oauth2' | 'openIdConnect';
+    description?: string;
+    name?: string;
+    in?: 'query' | 'header' | 'cookie';
+    scheme?: string;
+    bearerFormat?: string;
+    flows?: Record<'implicit' | 'password' | 'clientCredentials' | 'authorizationCode', OAuthFlow>;
+    openIdConnectUrl?: string;
+}
+
+interface OAuthFlow {
+    authorizationUrl: string;
+    tokenUrl: string;
+    refreshUrl?: string;
+    scopes: Record<string, string>;
+}
+
 export class Swagger {
     private doc: any;
     private baseUrl: string;
@@ -171,28 +209,145 @@ export class Swagger {
     info: Info;
     servers: Server[];
     paths: Record<string, PathItem>;
-    components: any;
-    security: any;
+    components: Components;
+    security: Record<string, string[]>;
     tags: Tag[];
     externalDocs: ExternalDoc;
 
-    constructor(baseUrl: string) {
-        this.baseUrl = baseUrl;
-        this.info = {
-            title: '',
-            version: ''
-        };
-        this.servers = [];
-        this.paths = {};
+    generate(schemas: Schema[], version: string): void {
+        this.info = this.generateInfo(version);
+        this.servers = this.generateServers();
+        this.paths = this.generatePaths(schemas);
+        this.components = this.generateComponent(schemas);
+        this.security = this.generateSecurity();
 
         this.doc = {
-            openapi: this.openapi,
-            servers: this.servers,
+            openapi: '3.0.0',
+            info: this.info,
+            server: this.servers,
             paths: this.paths,
+            components: this.components,
+            security: this.security
         };
     }
 
-    addServers(server: Server): void {
-        this.servers = this.servers.concat(server);
+    getJSON(): string {
+        return JSON.stringify(this.doc);
+    }
+
+    private generateInfo(version: string): Info {
+        let appName: string = PropertiesService.getScriptProperties().getProperty('app.name') || 'Sheet API';
+        let obj: Info = {
+            title: `Swagger ${appName}`,
+            version: version
+        };
+        return obj;
+    }
+
+    private generateServers(): Server[] {
+        let url = ScriptApp.getService().getUrl();
+        let server: Server = {
+            url: `${url}?url=/api/v1`
+        };
+        return [server];
+    }
+
+    private generatePaths(recs: Schema[]): Record<string, PathItem> {
+        let paths: Record<string, PathItem> = {};
+        let types = recs.map(r => r.resource).filter((value, index, self) => self.indexOf(value) === index);
+        types.forEach(type => {
+            let items = recs.filter(r => r.resource === type);
+            let pathItem: PathItem = {};
+            pathItem.get = this.createReadOperation(type, items);
+            paths[`/${type}`] = pathItem;
+        });
+        return paths;
+    }
+
+    private createReadOperation(resource: string, recs: Schema[]): Operation {
+        let op: Operation = {
+            parameters: [],
+            responses: {}
+        };
+        op.parameters.push(this.createParameter('offset', 'query', 'integer', 0));
+        op.parameters.push(this.createParameter('limit', 'query', 'integer', 20));
+        op.parameters.push(this.createParameter('where', 'query', 'string'));
+        op.parameters.push(this.createParameter('order', 'query', 'string'));
+        op.responses['200'] = {
+            description: 'Successful operation',
+            content: {
+                'application/json': {
+                    schema: {
+                        $ref: `#/components/schemas/${resource}`
+                    }
+                }
+            }
+        }
+        return op;
+    }
+
+    private generateComponent(recs: Schema[]): Components {
+        let schemas: Record<string, SchemaObject> = {};
+        let parameters: Record<string, Parameter>;
+        let requestBodies: Record<string, RequestBody>;
+        let securitySchemes: Record<string, SecurityScheme>;
+        let types = recs.map(r => r.resource).filter((value, index, self) => self.indexOf(value) === index);
+        types.forEach(type => {
+            let items = recs.filter(r => r.resource === type);
+            let requiredProps = items.map(i => i.validation).filter(vi => vi && !vi.includes('optional'));
+            let schema: SchemaObject = {
+                type: 'object',
+                properties: {}
+            };
+            if (requiredProps.length)
+                schema.required = requiredProps;
+            items.forEach(i => schema.properties[i.alias] = this.createSchemaObject(i));
+            schemas[type] = schema;
+        });
+        return {
+            schemas: schemas,
+            // parameters: parameters,
+            // requestBodies: requestBodies,
+            // securitySchemes: securitySchemes
+        };
+    }
+
+    private createSchemaObject(rec: Schema): SchemaObject {
+        let prop: SchemaObject = {};
+        switch (rec.type) {
+            case 'boolean':
+                prop.type = 'boolean';
+                break;
+            case 'number':
+                prop.type = prop.format ? 'number' : 'integer';
+                break;
+            default:
+                prop.type = 'string';
+                break;
+        }
+        if (rec.default)
+            prop.default = rec.default;
+        return prop;
+    }
+
+    private createParameter(name: string, location: InType, type: DataType, defValue?: any, required: boolean = false): Parameter {
+        let param: Parameter = {
+            name: name,
+            in: location,
+            required: required,
+            schema: {
+                type: type
+            }
+        };
+        if (defValue)
+            (param.schema as SchemaObject).default = defValue;
+        return param;
+
+    }
+
+    private generateSecurity(): Record<string, string[]> {
+        return {
+            api_key: []
+        }
     }
 }
